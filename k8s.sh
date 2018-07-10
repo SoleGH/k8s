6308385ca339b8ef6af4e2feb723c34b1636bf28
SoleGH <ybifei@163.com>
2018-07-10 10:02:01 +0800

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -5,11 +5,21 @@ KUBE_PAUSE_VERSION=3.1
 ETCD_VERSION=3.2.18
 DNS_VERSION=1.1.3
 
-GCR_URL=gcr.io/google_containers
-INLAND_URL=anjia0532
-MY_URL=registry.cn-hangzhou.aliyuncs.com/yangbf
+GCR_URL=gcr.io
+INLAND_URL=
+MY_ALI_URL=registry.cn-hangzhou.aliyuncs.com/yangbf
+MY_DOCKER_URL=soleyang
 
-images=(kube-proxy-amd64:${KUBE_VERSION}
+SOURCE_URL=$MY_DOCKER_URL
+AIM_URL=$MY_ALI_URL
+
+log_url='--username=997021985@qq.com registry.cn-hangzhou.aliyuncs.com'
+# log_url = soleyang
+echo $log_url
+docker login $log_url
+
+images=(
+kube-proxy-amd64:${KUBE_VERSION}
 kube-scheduler-amd64:${KUBE_VERSION}
 kube-controller-manager-amd64:${KUBE_VERSION}
 kube-apiserver-amd64:${KUBE_VERSION}
@@ -18,15 +28,17 @@ etcd-amd64:${ETCD_VERSION}
 coredns:${DNS_VERSION}
 #k8s-dns-sidecar-amd64:${DNS_VERSION}
 #k8s-dns-kube-dns-amd64:${DNS_VERSION}
-#k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION})
+#k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION}
+)
 
-docker login
-for imageName in ${images[@]} ; do
-  docker pull $INLAND_URL/$imageName
-  docker tag $INLAND_URL/$imageName $GCR_URL/$imageName
+for imageName in ${images[@]} 
+do
+  docker pull $SOURCE_URL/$imageName
+  docker tag $SOURCE_URL/$imageName $GCR_URL/$imageName
+  docker tag $SOURCE_URL/$imageName $AIM_URL/$imageName
   
-  docker push $MY_URL/$imageName
+  docker push $AIM_URL/$imageName
   # delete useless images
-  docker rmi $MY_URL/$imageName
-  docker rmi $INLAND_URL/$imageName
+  docker rmi $AIM_URL/$imageName
+  docker rmi $SOURCE_URL/$imageName
 done
